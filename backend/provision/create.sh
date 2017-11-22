7628c2e5dc973f75bd7a16723ccd1e79e84e6d3b
gogsadmin <RedHat$1>
2017-11-22 11:49:37 +0100

    build

---
+++ backend/provision/create.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#az login
+az account list -o table
+echo "please select subscription: "
+read sub
+az account set --subscription $sub
+
+
+
+echo "solution name: "
+read solname
+echo "password used: "
+read password 
+
+
+file=$(az ad sp create-for-rbac -n $solname-provision --role="Contributor" --scopes /subscriptions/$sub/resourceGroups/$solname --create-cert | jq -r '.fileWithCertAndPrivateKey')
+mv $file cert.pem
+
+tenantId=$(az account show | jq -r '.tenantId')
+
+# Create Dockerfile
+cat Dockerfile.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname'/' >Dockerfile
+sed -in 's/XXX_PASSWORD_XXX/'$password'/' Dockerfile
+sed -in 's/XXX_TENANTID_XXX/'$tenantId'/' Dockerfile
+
+# Create login script
+cat login.sh.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname-provision'/' >login.sh
+sed -in 's/XXX_TENANTID_XXX/'$tenantId'/' login.sh
+chmod 750 login.sh
+
+
+docker build -t $solname:1.0 .
+
+#az login --service-principal -u http://YOURAPPID -p /root/LOCATIONOFYOUR.PEMFILE --tenant TENANTID
--- backend/provision/provision.py
+++ backend/provision/provision.py
@@ -5,6 +5,8 @@ from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.keyvault import KeyVaultManagementClient
 from azure.keyvault import KeyVaultClient,KeyVaultId
 
+solname=os.getenv('AZURE_USER').replace('http://','')
+
 credentials = ServicePrincipalCredentials(
     client_id=os.getenv('AZURE_USER'),
     secret=os.getenv('AZURE_PWD'),
@@ -14,7 +16,7 @@ credentials = ServicePrincipalCredentials(
 client = KeyVaultClient(
     credentials
 )
-KEYVAULT='https://vmcr8tester.vault.azure.net'
+KEYVAULT='https://'+solname+'.vault.azure.net'
 secret_bundle = client.set_secret(KEYVAULT, 'dummy', '')
 secret_id = KeyVaultId.parse_secret_id(secret_bundle.id)
 redis_secret=client.get_secret(KEYVAULT, 'vmcr8tester-redis-pwd',secret_id.version_none).value
@@ -22,5 +24,5 @@ redis_secret=client.get_secret(KEYVAULT, 'vmcr8tester-redis-pwd',secret_id.versi
 key = sys.argv[1]
 value = sys.argv[2]
 
-r = redis.StrictRedis(host='vmcr8tester.redis.cache.windows.net', port=6380, db=0, password=redis_secret, ssl=True)
+r = redis.StrictRedis(host=solname+'.redis.cache.windows.net', port=6380, db=0, password=redis_secret, ssl=True)
 r.set(key, value)
