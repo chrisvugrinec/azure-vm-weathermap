89fddba068befd760bb9b98498119379f1623721
gogsadmin <RedHat$1>
2017-11-22 22:19:17 +0100

    fixed

--- backend/vmcr8tester/create.sh
+++ backend/vmcr8tester/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#az login
+az login
 az account list -o table
 echo "please select subscription: "
 read sub
@@ -11,11 +11,9 @@ read solname
 echo "password used: "
 read password 
 
-
-file=$(az ad sp create-for-rbac -n $solname-provision --role="Contributor" --scopes /subscriptions/$sub/resourceGroups/$solname --create-cert | jq -r '.fileWithCertAndPrivateKey')
-mv $file cert.pem
-
 tenantId=$(az account show | jq -r '.tenantId')
+file=$(az ad sp create-for-rbac -n $solname-create --role="Contributor" --scopes /subscriptions/$sub/resourceGroups/$solname --create-cert | jq -r '.fileWithCertAndPrivateKey')
+mv $file cert.pem
 
 # Create Dockerfile
 cat Dockerfile.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname'/' >Dockerfile
@@ -23,11 +21,13 @@ sed -in 's/XXX_PASSWORD_XXX/'$password'/' Dockerfile
 sed -in 's/XXX_TENANTID_XXX/'$tenantId'/' Dockerfile
 
 # Create login script
-cat login.sh.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname-provision'/' >login.sh
+cat login.sh.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname-create'/' >login.sh
 sed -in 's/XXX_TENANTID_XXX/'$tenantId'/' login.sh
 chmod 750 login.sh
 
+# Create required shell script
+cat createvm.sh.example | sed  's/XXX_SOLUTION_NAME_XXX/'$solname'/' >createvm.sh
 
-docker build -t $solname-provision:1.0 .
+docker build -t $solname-create:1.0 .
 
 #az login --service-principal -u http://YOURAPPID -p /root/LOCATIONOFYOUR.PEMFILE --tenant TENANTID
